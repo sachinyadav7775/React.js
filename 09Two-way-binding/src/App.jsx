93b35a2c82d6222aeaaf2e8c87b936c99750713b
sachin yadav <sachinyadav63928@gmail.com>
2026-07-22 13:29:26 +0530

    add new file

---
+++ 09Two-way-binding/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [title, setTitle] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('Form Submitted by', title);
+    setTitle('')
+  }
+  return (
+    <div>
+      <form onSubmit={ (e) => {submitHandler (e)} }>
+        <input 
+          type="text" 
+          placeholder='Enter Your Name'
+          value={title} 
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
