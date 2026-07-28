e7c36329685eef7291942033ecf0ec59df17bd5a
sachin yadav <sachinyadav63928@gmail.com>
2026-07-28 09:27:42 +0530

    add new file

---
+++ 19Context-API/src/components/Navber.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navber2 from './Navber2'
+
+const Navber = () => {
+  return (
+    <div className='nav'>
+        <h1>@Sachin_SKY</h1>
+        <Navber2/>
+    </div>
+  )
+}
+
+export default Navber
---
+++ 19Context-API/src/components/Navber2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navber2 = () => {
+  return (
+    <div className='nav2'>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Contact</h4>
+        <h4>Services</h4>
+    </div>
+  )
+}
+
+export default Navber2
