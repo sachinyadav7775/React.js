2f5635b82301d1b2c5903029c100d85714cd63fd
sachin yadav <sachinyadav63928@gmail.com>
2026-08-25 14:27:41 +0530

    complete project

--- Project/src/components/Dashboard/EmployeeDashboard.jsx
+++ Project/src/components/Dashboard/EmployeeDashboard.jsx
@@ -5,7 +5,7 @@ import TaskList from "../TaskList/TaskList"
 
 const EmployeeDashboard = (props) => {
   return (
-    <div className="h-screen p-15 bg-[#1C1C1C]">
+    <div className="h-screen lg:p-20 p-10 bg-[#1C1C1C]">
       <Header changeUser={props.changeUser} data={props.data} />
 
       <TaskListNumber data={props.data} />
